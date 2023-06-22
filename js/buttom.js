93acff5fd58dfe6d509258c5c66f8567ed8cd17e
Kathryne-Kov <112563496+Kathryne-Kat@users.noreply.github.com>
2023-06-22 21:01:25 +0300

    add fix

--- js/buttom.js
+++ js/buttom.js
@@ -1,14 +1,14 @@
-(() => {
-  window.addEventListener("scroll", function () {
-    var button = document.getElementById("fixedButton");
-    var rect = button.getBoundingClientRect();
-    var viewportHeight =
-      window.innerHeight || document.documentElement.clientHeight;
+// (() => {
+window.addEventListener("scroll", function () {
+  const button = document.getElementById("fixedButton");
+  const rect = button.getBoundingClientRect();
+  const viewportHeight =
+    window.innerHeight || document.documentElement.clientHeight;
 
-    if (rect.bottom >= viewportHeight) {
-      button.style.bottom = "10px"; // Оставляем небольшой отступ от нижнего края
-    } else {
-      button.style.bottom = "0";
-    }
-  });
-})();
+  if (rect.bottom >= viewportHeight) {
+    button.style.bottom = "10px"; // Оставляем небольшой отступ от нижнего края
+  } else {
+    button.style.bottom = "0";
+  }
+});
+// })();
